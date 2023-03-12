5c61a32abf21eb5c6f9b517c8ceb2b59b491759f
navvvgupta <abhi2017gupta@gmail.com>
2023-03-12 13:19:37 +0530

    Initial Code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import './index.css'
+import img1 from './Images/book_1.jpg'
+import img2 from './Images/book_2.jpg'
+const books=[
+    {
+    author:'James Clear',
+    title:'Atomic Habits: The life-changing Book for common men',
+    img_src: img1,
+    id:1
+    },
+    {
+        author:'Perana Ludho',
+        title:'Shark Tank India:The future Startup Culture of India',
+        img_src:img2,
+        id:2
+    }
+]
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList(){
+    const findBook=(id)=>{
+        const book=books.find((book)=> book.id===id)
+        console.log(book);
+    }
+    return(
+        <section className="booklist">
+           
+            {
+                books.map((book)=>{
+                    return <Book img_src={book.img_src} title={book.title} author={book.author} id={book.id} findBook={findBook} />
+                })
+            }
+        </section>
+    )
+}
+
+const Book =(props)=> {
+    const getSingleBook=()=>{
+        props.findBook(props.id)
+    }
+
+    return(
+        <section className="book">
+        <img src={props.img_src} alt="" />
+        <h2>{props.title}</h2>
+        <h4>{props.author}</h4>
+        <button onClick={getSingleBook}>CLick Here</button>
+        <span className="product-id">{props.id}</span>
+        </section>
+    )
+}
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<BookList/>)
